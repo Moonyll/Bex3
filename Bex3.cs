caf2cbb769aa2cae9b2d4e27e8c5f5b7b159b4d9
Moonyll <s.pouwel@laposte.net>
2018-12-18 12:55:28 +0100

    Bex3

---
+++ Bex3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombre;
+            for (nombre = 0; nombre < 10; nombre++)
+            {
+                Console.WriteLine("Bonjour, je suis un message généré à l'aide d'une boucle.");
+            }
+        }
+    }
+}
